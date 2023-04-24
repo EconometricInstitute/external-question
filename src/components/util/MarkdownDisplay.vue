247685882489f067b1c1db88b52ba8a2317ef670
Paul Bouman <bouman@ese.eur.nl>
2023-04-24 10:03:10 +0200

    Corrected the header of many files

--- src/components/util/MarkdownDisplay.vue
+++ src/components/util/MarkdownDisplay.vue
@@ -1,13 +1,23 @@
 <!--
-This file is part of External-Question.
-
-External-Question is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-External-Question is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along with External-Question. If not, see https://www.gnu.org/licenses/
+ * This file is part of External-Question.
+ * It is a single page web static web-application for advanced exercises related to computer skills.
+ * 
+ * Copyright (C) 2023 Paul Bouman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/
+ *
 -->
-
 
 <template>
   <div class="adjust-md" ref="renderDiv" v-html="rendered" />
