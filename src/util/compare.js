35eb6d8429b4c3d35e4b6d78a301d3a578c19278
Paul Bouman <bouman@ese.eur.nl>
2023-04-24 09:58:26 +0200

    Introduce the AGPL-3.0 license to the code base

--- src/util/compare.js
+++ src/util/compare.js
@@ -1,3 +1,24 @@
+/*
+ * This file is part of External-Question.
+ * It is a single page web static web-application for advanced exercises related to computer skills.
+ * 
+ * Copyright (C) 2023 Paul Bouman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/
+ *
+ */
+
 export default function compare(obj1, obj2, decimals, convert=false)
 {
     // If one object is not truthy and the other one is, they are not equal
--- src/util/hashtools.js
+++ src/util/hashtools.js
@@ -1,3 +1,24 @@
+/*
+ * This file is part of External-Question.
+ * It is a single page web static web-application for advanced exercises related to computer skills.
+ * 
+ * Copyright (C) 2023 Paul Bouman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/
+ *
+ */
+
 import { Base64 } from 'js-base64'
 import pako from 'pako';
 
